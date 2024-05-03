d354c615d6fb2ec6895c37b328a955d433c67693
developwithhammad <hammadquiz@gmail.com>
2024-05-03 19:29:24 +0500

    day 6 completed

--- app.js
+++ app.js
@@ -43,7 +43,7 @@
 //     str = str.map((currWord) =>
 //         // Method 1 for uppercase
 //         // currWord.replace(currWord[0], currWord[0].toUpperCase())
-//         // Method 2 for uppercase 
+//         // Method 2 for uppercase
 //         currWord.charAt(0).toUpperCase() + currWord.slice(1)
 //     )
 //     str = `#${str.join("")}`;
@@ -53,6 +53,7 @@
 
 
 // ========== DAY 3 ========== //
+// Count how many I in missIssippi
 
 // function countChar(word, char) {
 //     var word = word.toLowerCase();
@@ -70,16 +71,66 @@
 // countChar("MissIssippi", "I")
 
 
-function findTheSidesOfTriangle(a, b, c) {
-    if (a === b && b == c) {
-        return "equilateral"
+
+// ==== ****** DAY 4 ****** ==== //
+// Identify whether the each side of triangle equal or what or identify what type of triangle is this
+
+// function findTheSidesOfTriangle(a, b, c) {
+//     if (a === b && b == c) {
+//         return "equilateral"
+//     }
+//     if (a === b || b == c || a === c) {
+//         return "isosceles"
+//     }
+//     return "scalene"
+// }
+
+// console.log(findTheSidesOfTriangle(3, 3, 3));
+// console.log(findTheSidesOfTriangle(3, 3, 4));
+// console.log(findTheSidesOfTriangle(3, 4, 5));
+
+
+
+
+// ==== ****** DAY 5 ****** ==== //
+// set the given characters or numbers in ascending order
+
+// const randNumsArr = [2, 4, 7, 1];
+
+// function sortAscending(arr) {
+//     return arr.sort((a, b) => a - b)
+// }
+
+// console.log(randNumsArr)
+// console.log(sortAscending(randNumsArr))
+
+// let strArr = ["3", "1", "8", "6", "7", "2"];
+// console.log(strArr)
+
+// function sortAscending(str) {
+//     const sortedString = str.sort((a, b) => a - b)
+//     console.log(sortedString);
+// }
+
+// sortAscending(strArr);
+
+
+
+// ==== ****** DAY 5 ****** ==== //
+// Match that given words are palindrome or not
+
+const input = prompt("Enter any word to check whether it is palindrome or not");
+
+function isPalindrome(str) {
+    const strInSmall = str.toLowerCase()
+    const reverseStr = str.split("").reverse().join("");
+    if (strInSmall === reverseStr) {
+        console.log(`${strInSmall} and ${reverseStr} is palindrome`)
     }
-    if (a === b || b == c || a === c) {
-        return "isosceles"
+    else {
+        console.log(`${strInSmall} and ${reverseStr} is not palindrome`)
     }
-    return "scalene"
+
 }
 
-console.log(findTheSidesOfTriangle(3, 3, 3));
-console.log(findTheSidesOfTriangle(3, 3, 4));
-console.log(findTheSidesOfTriangle(3, 4, 5)); 
+isPalindrome(input)
